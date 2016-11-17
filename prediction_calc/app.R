96850e6c0e8fa8a828eb80f7088d5ec77def3b68
James Dayhuff <home@Jamess-MacBook-Pro.local>
2016-11-16 21:18:45 -0700

    first big changes

--- prediction_calc/app.R
+++ prediction_calc/app.R
@@ -11,37 +11,41 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
+  
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Hello Shiny!"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+        selectInput("term", 
+                  label = "Term Length", 
+                  choices = list("36 months" = "36 months", "60 months" = "60 months"), 
+                  selected = 1),
+        textInput("my_text", 
+                  label = "Text input",
+                  value = "Enter text..."),
+        numericInput("my_num",
+                  label = "Numeric input", 
+                  value = 1),
+        submitButton(text="Submit"),
+        actionButton("do", "Click Me")
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+        textOutput("text1")
       )
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+
+   output$text1 <- renderText({
+     paste("You have selected ", input$term)
    })
 }
 
